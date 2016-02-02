426574aa5738b26f64129c302e7438f4c9b519ab
Kamil Sorokosz <mail@mail>
2016-02-02 22:25:11 +0100

    Code and presentations added

---
+++ Kod/Basic/komunikacja.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string name;
+	cout << "What's your name?" << endl;
+	cin >> name;
+	cout << "Hi " << name << endl;
+
+	return 0;
+}
